72d219206e9c059c4b709e45eead15af49b2d018
Maximilian Schmidt <landlaeufer@gmail.com>
2022-08-08 11:14:09 +0200

    Updated tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,38 +6,39 @@ from contextlib import suppress, AsyncExitStack, asynccontextmanager
 import pytest
 from codestare.async_utils import TaskNursery, CoroutineWrapper
 
-pytestmark = pytest.mark.asyncio
-
 
 class MockWrapper(CoroutineWrapper):
-    def __init__(self, range: int | None = 20):
+    def __init__(self, range: int | None = 20, sleep_time=0.1):
         self.value = None
         self.range = range
-        super().__init__(coroutine=self._run())
+        super().__init__(coroutine=self._run(sleep_time))
 
-    async def _run(self):
+    async def _run(self, sleep_time):
+        self.value = 0
+
         if self.range is not None:
             for i in range(self.range):
                 self.value = i
-
+                await asyncio.sleep(sleep_time)
         else:
             while True:
                 self.value += 1
+                await asyncio.sleep(sleep_time)
 
 
 @pytest.fixture
-async def nursery(request):
+async def nursery() -> TaskNursery:
     callback_ran = False
 
     async def callback():
         nonlocal callback_ran
         callback_ran = True
 
-    nursery = request.param() if hasattr(request, 'param') else TaskNursery()
-    nursery.add_sentinel_callback(callback())
+    nursery = TaskNursery()
+    nursery.push_async_callback(callback)
     yield nursery
 
-    if any(t.cancelled() for t in nursery._tasks):
+    if any(task.cancelled() for task in nursery.tasks):
         assert callback_ran
 
 
@@ -52,7 +53,7 @@ async def test_nursery(future, nursery):
 
     task = nursery.create_task(future)
     task.add_done_callback(callback)
-    task.add_done_callback(lambda _: nursery.trigger_sentinel.set())
+    task.add_done_callback(lambda _: nursery.sentinel_task.cancel())
 
     task.cancel()
     with suppress(asyncio.CancelledError):
@@ -61,52 +62,37 @@ async def test_nursery(future, nursery):
     assert callback_called
 
 
-class MockNursery(TaskNursery):
-    def __init__(self):
-        super().__init__()
-        self.exit_stack = AsyncExitStack()
-        self._task = None
-        self.add_sentinel_callback(self.exit_stack.aclose())
-        self._context: t.AsyncContextManager = self.__make_context()
-
-    def run(self):
-        if not self._task:
-            self._task = self.create_task(MockWrapper(range=None))
-            self._task.add_done_callback(lambda _: self.trigger_sentinel.set())
-
-        return self._task
-
-    @asynccontextmanager
-    async def __make_context(self):
-        task = self.run()
-        yield
-        task.cancel()
-        with suppress(asyncio.CancelledError):
-            await task
-
-    def __aenter__(self):
-        return self._context.__aenter__()
-
-    def __aexit__(self, exc_type, exc_val, exc_tb):
-        return self._context.__aexit__(exc_type, exc_val, exc_tb)
-
-
-@pytest.mark.parametrize('nursery', [MockNursery], indirect=True)
-async def test_exit_stack(nursery: MockNursery):
+async def test_exit_stack(nursery):
     value = None
 
     @asynccontextmanager
-    async def test_context_manager():
+    async def t_context_manager():
         nonlocal value
         value = True
         yield
         value = False
 
     async with nursery:
-        await nursery.exit_stack.enter_async_context(test_context_manager())
+        await nursery.enter_async_context(t_context_manager())
 
-    assert nursery.run().cancelled()
-    assert nursery.trigger_sentinel.is_set()
+    assert nursery.sentinel_task is None
     await asyncio.sleep(0)
     assert value is not None, "context was not entered"
     assert value is False, "context was not exited"
+
+
+@pytest.mark.parametrize('future', [MockWrapper(range=None)])
+async def test_swap_ctx_manager(nursery, future):
+    async with nursery:
+        task = nursery.create_task(future)
+        new = nursery.pop_all()
+
+    assert nursery.sentinel_task is not None
+    assert not task.cancelled()
+
+    await new.aclose()
+    assert task.cancelled()
+    assert nursery.sentinel_task is None
+
+
+
